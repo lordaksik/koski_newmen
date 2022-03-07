5878a50a832a1068b7bac0e93313f9c2e1cfdf18
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-03-07 03:56:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,130 @@ bot.hears('/bot', async (ctx) => {
       const response = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/10/1/")
       const data = await response.json()
       var res=0,total=0;
+      var n1=0,n2=0,n3=0,n4=0,n5=0,n6=0;
+      var a1=0,a2=0,a3=0,a4=0,a5=0,a6=0;
+      var b1=0,b2=0,b3=0,b4=0,b5=0,b6=0;
       var res6=0,res7=0,res8=0,res9=0;
+      
+      for (let i = 0; i <= 14; i++) {
+         number = data.items.results[i].results[0].number
+         number2 = data.items.results[i].results[1].number
+         if(parseInt(number,10)!=1){
+         a1++;
+         }
+          if(parseInt(number,10)!=2){
+         a2++;
+         }
+          if(parseInt(number,10)!=3){
+         a3++;
+         }
+          if(parseInt(number,10)!=4){
+         a4++;
+         }
+          if(parseInt(number,10)!=5){
+         a5++;
+         }
+          if(parseInt(number,10)!=6){
+         a6++;
+         }
+           if(parseInt(number2,10)!=1){
+         b1++;
+         }
+          if(parseInt(number2,10)!=2){
+         b2++;
+         }
+          if(parseInt(number2,10)!=3){
+         b3++;
+         }
+          if(parseInt(number2,10)!=4){
+         b4++;
+         }
+          if(parseInt(number2,10)!=5){
+         b5++;
+         }
+          if(parseInt(number2,10)!=6){
+         b6++;
+         }
+      }
+      if(a1===15){
+         ctx.reply("1 у красной кости не было  15 раз подряд");
+      }
+       if(a2===15){
+         ctx.reply("2 у красной кости не было  15 раз подряд");
+      }
+       if(a3===15){
+         ctx.reply("3 у красной кости не было  15 раз подряд");
+      }
+       if(a4===15){
+         ctx.reply("4 у красной кости не было  15 раз подряд");
+      }
+       if(a5===15){
+         ctx.reply("5 у красной кости не было  15 раз подряд");
+      }
+       if(a1===15){
+         ctx.reply("1 у красной кости не было  15 раз подряд");
+      }
+       if(b1===15){
+         ctx.reply("1 у синей кости не было  15 раз подряд");
+      }
+       if(b2===15){
+         ctx.reply("2 у синей кости не было  15 раз подряд");
+      }
+       if(b3===15){
+         ctx.reply("3 у синей кости не было  15 раз подряд");
+      }
+       if(b4===15){
+         ctx.reply("4 у синей кости не было  15 раз подряд");
+      }
+       if(b5===15){
+         ctx.reply("5 у синей кости не было  15 раз подряд");
+      }
+       if(b6===15){
+         ctx.reply("6 у синей кости не было  15 раз подряд");
+      }
+      
+      
+      for (let i = 0; i <= 29; i++) {
+         number = data.items.results[i].results[0].number
+         number2 = data.items.results[i].results[1].number
+         if(parseInt(number,10)!=1 && parseInt(number2,10)!=1){
+         n1++;
+         }
+          if(parseInt(number,10)!=2 && parseInt(number2,10)!=2){
+         n2++;
+         }
+          if(parseInt(number,10)!=3 && parseInt(number2,10)!=3){
+         n3++;
+         }
+          if(parseInt(number,10)!=4 && parseInt(number2,10)!=4){
+         n4++;
+         }
+          if(parseInt(number,10)!=5 && parseInt(number2,10)!=5){
+         n5++;
+         }
+          if(parseInt(number,10)!=6 && parseInt(number2,10)!=6){
+         n6++;
+         }
+      }
+      if(n1===30){
+         ctx.reply("1-1 не было,  30 раз подряд");
+      }
+      if(n2===30){
+         ctx.reply("2-2 не было,  30 раз подряд");
+      }
+      if(n3===30){
+         ctx.reply("3-3 не было,  30 раз подряд");
+      }
+      if(n4===30){
+         ctx.reply("4-4 не было,  30 раз подряд");
+      }
+      if(n5===30){
+         ctx.reply("5-5 не было,  30 раз подряд");
+      }
+      if(n6===30){
+         ctx.reply("6-6 не было,  30 раз подряд");
+      }
+      
       for (let i = 0; i <= 11; i++) {
          number = data.items.results[i].results[0].number
          number2 = data.items.results[i].results[1].number

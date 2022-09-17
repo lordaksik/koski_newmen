706c41d2f864e03646a8a7436d09b15cb2d3e3ad
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-09-17 14:42:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ bot.start((ctx) => {
 bot.help((ctx) => ctx.reply('Напиши /bot '))
 bot.hears('/bot', async (ctx) => {
    async function request2() {
-      
+       try{
       const response = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/10/1/")
       const data = await response.json()
       const response2 = await fetch("https://betgames9.betgames.tv/web/v2/games/results/testpartner/en/0/2020-27-07/10/2/")
@@ -104,49 +104,94 @@ bot.hears('/bot', async (ctx) => {
          for (let i = 0; i <= 29; i++) {
          numb = data4.items.results[i].results[0].number
          numb2 = data4.items.results[i].results[1].number
-         if(parseInt(numb,10)!=1 && parseInt(numb2,10)!=1){
+         if(parseInt(numb,10)===1 && parseInt(numb2,10)===1){
          l1++;
          }
-          if(parseInt(numb,10)!=2 && parseInt(numb2,10)!=2){
+          if(parseInt(numb,10)===2 && parseInt(numb2,10)===2){
          l2++;
          }
-          if(parseInt(numb,10)!=3 && parseInt(numb2,10)!=3){
+          if(parseInt(numb,10)===3 && parseInt(numb2,10)===3){
          l3++;
          }
-          if(parseInt(numb,10)!=4 && parseInt(numb2,10)!=4){
+          if(parseInt(numb,10)===4 && parseInt(numb2,10)===4){
          l4++;
          }
-          if(parseInt(numb,10)!=5 && parseInt(numb2,10)!=5){
+          if(parseInt(numb,10)===5 && parseInt(numb2,10)===5){
          l5++;
          }
-          if(parseInt(numb,10)!=6 && parseInt(numb2,10)!=6){
+          if(parseInt(numb,10)===6 && parseInt(numb2,10)===6){
          l6++;
          }
       }
+      for (let i = 0; i <= 29; i++) {
+         numb = data5.items.results[i].results[0].number
+         numb2 = data5.items.results[i].results[1].number
+         if(parseInt(numb,10)===1 && parseInt(numb2,10)===1){
+         l11++;
+         }
+          if(parseInt(numb,10)===2 && parseInt(numb2,10)===2){
+         l12++;
+         }
+          if(parseInt(numb,10)===3 && parseInt(numb2,10)===3){
+         l13++;
+         }
+          if(parseInt(numb,10)===4 && parseInt(numb2,10)===4){
+         l14++;
+         }
+          if(parseInt(numb,10)===5 && parseInt(numb2,10)===5){
+         l15++;
+         }
+          if(parseInt(numb,10)===6 && parseInt(numb2,10)===6){
+         l16++;
+         }
+      }
       
+      
+      for (let i = 0; i <= 9; i++) {
+         numb = data6.items.results[i].results[0].number
+         numb2 = data6.items.results[i].results[1].number
+         if(parseInt(numb,10)===1 && parseInt(numb2,10)===1){
+         l21++;
+         }
+          if(parseInt(numb,10)===2 && parseInt(numb2,10)===2){
+         l22++;
+         }
+          if(parseInt(numb,10)===3 && parseInt(numb2,10)===3){
+         l23++;
+         }
+          if(parseInt(numb,10)===4 && parseInt(numb2,10)===4){
+         l24++;
+         }
+          if(parseInt(numb,10)===5 && parseInt(numb2,10)===5){
+         l25++;
+         }
+          if(parseInt(numb,10)===6 && parseInt(numb2,10)===6){
+         l26++;
+         }
+      }
       console.log('нет 1  ' +n1+'  '+n11+'  '+n12);
       console.log('нет 2  ' +n2+'  '+n21+'  '+n22);
       console.log('нет 3  ' +n3+'  '+n31+'  '+n32);
       console.log('нет 4  ' +n4+'  '+n41+'  '+n42);
       console.log('нет 5  ' +n5+'  '+n51+'  '+n52);
       console.log('нет 6  ' +n6+'  '+n61+'  '+n62);
-      if(n1===0 && n11===0 && n12===0){
-         ctx.reply("1-1 не было,  90 раз подряд");
+      if(n1===0 && n11===0 && n12===0 && l1===0 && l11===0  && l21===0){
+         ctx.reply("1-1 не было,  160 раз подряд");
       }
-      if(n2===0 && n21===0 && n22===0){
-         ctx.reply("2-2 не было,  90 раз подряд");
+      if(n2===0 && n21===0 && n22===0  && l2===0  && l12===0  && l22===0){
+         ctx.reply("2-2 не было,  160 раз подряд");
       }
-      if(n3===0 && n31===0 && n32===0){
-         ctx.reply("3-3 не было,  90 раз подряд");
+      if(n3===0 && n31===0 && n32===0  && l3===0  && l13===0  && l23===0){
+         ctx.reply("3-3 не было,  160 раз подряд");
       }
-      if(n4===0 && n41===0 && n42===0){
-         ctx.reply("4-4 не было,  90 раз подряд");
+      if(n4===0 && n41===0 && n42===0  && l4===0  && l14===0  && l24===0){
+         ctx.reply("4-4 не было,  160 раз подряд");
       }
-      if(n5===0 && n51===0 && n52===0){
-         ctx.reply("5-5 не было,  90 раз подряд");
+      if(n5===0 && n51===0 && n52===0  && l5===0  && l15===0  && l25===0){
+         ctx.reply("5-5 не было,  160 раз подряд");
       }
-      if(n6===0 && n61===0 && n62===0){
-         ctx.reply("6-6 не было,  90 раз подряд");
+      if(n6===0 && n61===0 && n62===0  && l6===0   && l16===0  && l26===0){
+         ctx.reply("6-6 не было,  160 раз подряд");
       }
     
    }
@@ -157,6 +202,10 @@ bot.hears('/bot', async (ctx) => {
       global.time = setInterval(request2, 30000)
    }
    good()
+   }
+     catch (err) {
+       console.log(err)
+    }
 }
 )
 
